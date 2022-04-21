6fa14e7a4ae1012848ad05f9c8922c6a3615b81e
georgmaisser <georg.maisser@wunderbyte.at>
2022-04-21 09:29:00 +0200

    Version 1.1.7 (2022042100)

--- CHANGES.md
+++ CHANGES.md
@@ -1,2 +1,5 @@
+## Version 1.1.7 (2022042100)
+* Fix nolazyout function
+
 ## Version 1.1.6 (2022041800)
 * No login required for using lazyloading of Wunderbyte Table
--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
-$plugin->release = '1.1.6';
+$plugin->release = '1.1.7';
 $plugin->supported = [39, 311];
-$plugin->version = 2022041800;
+$plugin->version = 2022042100;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
