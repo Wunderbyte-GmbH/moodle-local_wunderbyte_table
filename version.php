6e844b5b9a0b677b9db922e5af154b4b4d435b21
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2023-01-31 14:43:59 +0100

    Layout fix for dropdown menus and version bump.

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
 $plugin->release = '1.4.5';
-$plugin->version = 2023012600;
+$plugin->version = 2023013100;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
