1727af88ca326156b8d5a5ff3161e0cac1823f75
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2023-03-21 10:33:19 +0100

    Bugfix: We want to keep 0 values, so we cannot use empty(...) here.

--- classes/wunderbyte_table.php
+++ classes/wunderbyte_table.php
@@ -968,7 +968,7 @@ class wunderbyte_table extends table_sql {
 
                 $row = (array)$row;
 
-                if (empty($row[$key])) {
+                if (!isset($row[$key]) || $row[$key] === null) {
                     continue;
                 }
 
