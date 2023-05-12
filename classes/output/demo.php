693476013eb6d51df8782ce31a1cfb51bfdce1b3
eynimeni <maganatte@yahoo.de>
2023-05-12 08:54:17 +0200

    work in progress - unable filter cashing and multitable for developing purpose

--- classes/output/demo.php
+++ classes/output/demo.php
@@ -537,14 +537,16 @@ class demo implements renderable, templatable {
      */
     public function return_as_array():array {
         $data = [
-            'table1' => $this->render_table_1(),
-            'tab1_name' => TABLE1NAME,
-            'table2' => $this->render_table_2(),
-            'tab2_name' => TABLE2NAME,
+            //'table1' => $this->render_table_1(),
+            //'tab1_name' => TABLE1NAME,
+            'table1' => $this->render_table_2(),
+            'tab1_name' => TABLE2NAME,
+            /*
             'table3' => $this->render_table_3(),
             'tab3_name' => TABLE3NAME,
             'table4' => $this->render_table_4(),
             'tab4_name' => TABLE4NAME,
+            */
 
         ];
 
