a3a6019695439dc11a6e50044052f4a6c3f28ed3
Georg Maißer <georg.maisser@wunderbyte.at>
2023-11-11 13:56:37 +0100

    add core_external to namespace

--- classes/external/execute_action.php
+++ classes/external/execute_action.php
@@ -29,10 +29,10 @@ namespace local_wunderbyte_table\external;
 
 use context_system;
 use Exception;
-use external_api;
-use external_function_parameters;
-use external_value;
-use external_single_structure;
+use core_external\external_api;
+use core_external\external_function_parameters;
+use core_external\external_value;
+use core_external\external_single_structure;
 use local_wunderbyte_table\wunderbyte_table;
 use moodle_url;
 
