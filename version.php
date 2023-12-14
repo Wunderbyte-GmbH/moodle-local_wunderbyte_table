5d98413920de26b555da4994548c53b8aa904f38
Georg Maißer <georg.maisser@wunderbyte.at>
2023-12-14 15:03:56 +0100

    Version 1.9.2 (2023121400)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
-$plugin->release = '1.9.1';
-$plugin->version = 2023121100;
+$plugin->release = '1.9.2';
+$plugin->version = 2023121400;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
