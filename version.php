c7a5b3b829aa2aad1fd15b41f9a1878d62f75c72
Georg Maißer <georg.maisser@wunderbyte.at>
2024-01-06 16:58:51 +0100

    Version 1.9.4 (2024010600)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
-$plugin->release = '1.9.3';
-$plugin->version = 2023122000;
+$plugin->release = '1.9.4';
+$plugin->version = 2024010600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
