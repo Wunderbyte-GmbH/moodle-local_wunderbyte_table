9b7916c73d2e8aad8c839fd513942f12e507d9c7
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2024-04-22 11:10:18 +0200

    Version 2.0.5 (2024042200)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
-$plugin->release = '2.0.4';
-$plugin->version = 2024041600;
+$plugin->release = '2.0.5';
+$plugin->version = 2024042200;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
