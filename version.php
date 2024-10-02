9858143e7cc15f5f99bfa050dfacae3e577ca73e
Georg Maißer <georg.maisser@wunderbyte.at>
2024-10-02 17:26:21 +0200

    Version (2024100200)

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
 $plugin->release = '2.0.16';
-$plugin->version = 2024100100;
+$plugin->version = 2024100200;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
