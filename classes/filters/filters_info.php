80feb04853f1c052a5f3edd3f013fff7bdc8a4ad
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2025-01-27 15:57:44 +0100

    Improvement: Add filter form

--- classes/filters/filters_info.php
+++ classes/filters/filters_info.php
@@ -24,8 +24,6 @@
 
 namespace local_wunderbyte_table\filters;
 
-use cache;
-use cache_helper;
 use coding_exception;
 use core_component;
 use dml_exception;
@@ -66,12 +64,13 @@ class filters_info {
         $filterobjects = $tablesettings['filtersettings'];
 
         foreach ($filterobjects as $key => $filter) {
-
             if ($key === 'id') {
-                $mform->addElement('advcheckbox',
+                $mform->addElement(
+                    'advcheckbox',
                     'id_wb_checked',
                     '',
-                    get_string('showfilter', 'local_wunderbyte_table'));
+                    get_string('showfilter', 'local_wunderbyte_table')
+                );
 
                     // We save the filterobject as we get it here.
                     $mform->addElement('hidden', 'wb_jsontablesettings', json_encode($tablesettings));
