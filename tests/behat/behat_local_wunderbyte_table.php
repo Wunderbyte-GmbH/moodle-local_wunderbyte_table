563c0fc38707467ebca058e01d3f8b91154c7cb3
Andrii Semenets <semteacher@gmail.com>
2025-03-27 20:38:33 +0200

    create test method to clean-up wbtable's cache

---
+++ tests/behat/behat_local_wunderbyte_table.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines message providers (types of messages being sent)
+ *
+ * @package local_wunderbyte_table
+ * @copyright 2025 Wunderbyte GmbH <info@wunderbyte.at>
+ * @author Andrii Semenets
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use Behat\Mink\Exception\UnsupportedDriverActionException;
+use Behat\Mink\Exception\DriverException;
+
+/**
+ * To create wunderbyte_table specific behat scearios.
+ */
+class behat_local_wunderbyte_table extends behat_base {
+    /**
+     * Clean bookig singleton cache
+     * @Given /^I clean wbtable cache$/
+     * @return void
+     */
+    public function i_clean_wbtable_cache() {
+        // Mandatory clean-up.
+        cache_helper::purge_by_event('changesinwunderbytetable');
+        $_POST = [];
+    }
+}
