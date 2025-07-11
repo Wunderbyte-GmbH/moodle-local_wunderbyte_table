f00658079b59e994dbd5505b6ded18a220e73cc2
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-07-11 13:25:20 +0200

    Version (2025071100)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wunderbyte_table';
 $plugin->release = '2.0.54';
-$plugin->version = 2025070800;
+$plugin->version = 2025071100;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->supported = [401, 405];
